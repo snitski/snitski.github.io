5af405ac4078c3a3fd22c13ecf02a2c8eb5394f7
Alex Snit <57328840+ajinoxx@users.noreply.github.com>
2021-02-22 20:13:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu()
+{
+    if (document.getElementById("sidebar").style.display == "none")
+        document.getElementById("sidebar").style.display = "block";
+    else
+        document.getElementById("sidebar").style.display = "none";
+}
