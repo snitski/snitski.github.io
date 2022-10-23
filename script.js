6924691635ef17087556f4bd1130367ea0a3c32c
snitski <jjsnit@gmail.com>
2022-10-23 03:09:29 -0400

    Remastered startpage

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function toggleMenu()
-{
-    const sidebar = document.getElementById("sidebar");
-
-    sidebar.setAttribute('class', sidebar.classList.contains('slide-in') ? 'slide-out' : 'slide-in')
+let handleClick = (event) => {
+    if(event.target.id !== "sidebar-toggle") 
+        return;
+    if(sidebar.classList.length === 0) 
+        sidebar.classList.add("slide-in")
+    else if(sidebar.classList.contains("slide-out")) 
+        sidebar.classList.replace("slide-out", "slide-in");
+    else
+        sidebar.classList.replace("slide-in", "slide-out");
 }
+var sidebar = document.getElementById("sidebar");
+window.addEventListener("click", handleClick);
